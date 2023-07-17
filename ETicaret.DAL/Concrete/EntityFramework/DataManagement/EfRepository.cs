2985d90940627fd93d308dc0c0d3ec7f92af4b61
salihcancakar <salihxcan@hotmail.com>
2023-07-17 08:38:55 +0300

    DB CRUD

---
+++ ETicaret.DAL/Concrete/EntityFramework/DataManagement/EfRepository.cs
@@ -0,0 +1,76 @@
+namespace ETicaret.DAL.Concrete.EntityFramework.DataManagement;
+
+using System.Linq.Expressions;
+using Abstract.DataManagement;
+using ETicaretAPI.Entity.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+public class EfRepository<T> : IRepository<T> where T : AuditableEntity
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public EfRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+        
+        
+    public async Task<T> GetAsync(Expression<Func<T, bool>> Filter, params string[] IncludeProperties)
+    {
+        IQueryable<T> query = _dbSet;
+        query = query.Where(Filter);
+        if (IncludeProperties.Length > 0)
+        {
+            foreach (var item in IncludeProperties)
+            {
+                query.Include(item);
+            }
+        }
+
+        return await query.SingleOrDefaultAsync();
+        
+        // return await query.FirstOrDefault -->> Bu kullanılabilir ama aralarındaki fark Eğer dönecek 2 veri varsa hatalı olanıda döndürebilir
+        // ama singleordefault hata fırlatır. SOD kullanmak daha mantıklı.
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> Filter = null, params string[] IncludeProperties)
+    {
+        IQueryable<T> query = _dbSet;
+
+        if (Filter != null)
+        {
+            query = query.Where(Filter);
+            
+        }
+
+        if (IncludeProperties.Length > 0)
+        {
+            foreach (var item in IncludeProperties)
+            {
+                query.Include(item);
+            }
+            
+        }
+
+        return await Task.Run(() => query);
+
+    }
+
+    public async Task<EntityEntry<T>> AddAsync(T Entity)
+    {
+        return await _dbSet.AddAsync(Entity);
+    }
+
+    public async Task UpdateAsync(T Entity)
+    {
+        await Task.Run(() => _dbSet.Update(Entity));
+    }
+
+    public async Task RemoveAsync(T Entity)
+    {
+        await Task.Run(() => _dbSet.Remove(Entity));
+    }
+}
---
+++ ETicaret.DAL/Concrete/EntityFramework/DataManagement/EfUnitOfWork.cs
@@ -0,0 +1,55 @@
+namespace ETicaret.DAL.Concrete.EntityFramework.DataManagement;
+
+using Abstract;
+using Abstract.DataManagement;
+using ETicaretAPI.Entity.Base;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+public class EfUnitOfWork : IUnitOfWork
+{
+
+    private readonly ETicaretContext _eTicaretContext;
+    private readonly IHttpContextAccessor _httpContextAccessor; 
+
+    public EfUnitOfWork(ETicaretContext eTicaretContext, IHttpContextAccessor httpContextAccessor)
+    {
+        _eTicaretContext = eTicaretContext;
+        _httpContextAccessor = httpContextAccessor;
+    } 
+    
+    public ICategoryRepository CategoryRepository { get; }
+    public IOrderRepository OrderRepository { get; }
+    public IOrderDetailRepository OrderDetailRepository { get; }
+    public IProductRepository ProductRepository { get; }
+    public IUserRepository UserRepository { get; }
+    public async Task<int> SaveChangeAsync()
+    {
+        foreach (var item in _eTicaretContext.ChangeTracker.Entries<AuditableEntity>())
+        {
+            if (item.State == EntityState.Added)
+            {
+                item.Entity.AddedTime = DateTime.UtcNow;
+                item.Entity.UpdatedTime = DateTime.UtcNow;
+                item.Entity.AddedUser = 1;
+                item.Entity.UpdatedTime = DateTime.UtcNow;
+                item.Entity.AddedIPV4Address = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+                item.Entity.UpdatedIPV4Address  = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                if (item.Entity.IsActive == null)
+                {
+                    item.Entity.IsActive = true;
+                }
+                item.Entity.IsDeleted = false;
+            }
+            else if (item.State == EntityState.Modified)
+            {
+                item.Entity.UpdatedTime = DateTime.UtcNow;
+                item.Entity.UpdatedUser = 1;
+                item.Entity.UpdatedIPV4Address = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+                
+            }
+        }
+        return await _eTicaretContext.SaveChangesAsync();
+    }
+}
